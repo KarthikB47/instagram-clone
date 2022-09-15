d2318d3ed49f995c220463881e29d4d2603b88e6
Karthik Balendran <karthik.bale47@gmail.com>
2022-09-15 19:11:44 +0530

    instagram clone

---
+++ stream_agram/lib/app/state/demo_users.dart
@@ -0,0 +1,64 @@
+import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
+
+/// Demo application users.
+enum DemoAppUser {
+  sahil,
+  sacha,
+  reuben,
+  gordon,
+}
+
+/// Convenient class Extension on [DemoAppUser] enum
+extension DemoAppUserX on DemoAppUser {
+  /// Convenient method Extension to generate an [id] from [DemoAppUser] enum
+  String? get id => {
+        DemoAppUser.sahil: 'sahil-kumar',
+        DemoAppUser.sacha: 'sacha-arbonel',
+        DemoAppUser.reuben: 'reuben-turner',
+        DemoAppUser.gordon: 'gordon-hayes',
+      }[this];
+
+  /// Convenient method Extension to generate a [name] from [DemoAppUser] enum
+  String? get name => {
+        DemoAppUser.sahil: 'Sahil Kumar',
+        DemoAppUser.sacha: 'Sacha Arbonel',
+        DemoAppUser.reuben: 'Reuben Turner',
+        DemoAppUser.gordon: 'Gordon Hayes',
+      }[this];
+
+  /// Convenient method Extension to generate [data] from [DemoAppUser] enum
+  Map<String, Object>? get data => {
+        DemoAppUser.sahil: {
+          'first_name': 'Sahil',
+          'last_name': 'Kumar',
+          'full_name': 'Sahil Kumar',
+        },
+        DemoAppUser.sacha: {
+          'first_name': 'Sacha',
+          'last_name': 'Arbonel',
+          'full_name': 'Sacha Arbonel',
+        },
+        DemoAppUser.reuben: {
+          'first_name': 'Reuben',
+          'last_name': 'Turner',
+          'full_name': 'Reuben Turner',
+        },
+        DemoAppUser.gordon: {
+          'first_name': 'Gordon',
+          'last_name': 'Hayes',
+          'full_name': 'Gordon Hayes',
+        },
+      }[this];
+
+  /// Convenient method Extension to generate a [token] from [DemoAppUser] enum
+  Token? get token => <DemoAppUser, Token>{
+        DemoAppUser.sahil: const Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjMyNDgxMTYsInVzZXJfaWQiOiJzYWhpbC1rdW1hciJ9.ARqx04IkcrBQFpmKK1aVdNnoLvxsxHayG955ZTFkYcs'),
+        DemoAppUser.sacha: const Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjMyNDgwOTIsInVzZXJfaWQiOiJzYWNoYS1hcmJvbmVsIn0.8ymERQJtDnR96cUtaeAU-BWX5UwZW8n-toEd5Yp18Ic'),
+        DemoAppUser.reuben: const Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjMyNDgxMDksInVzZXJfaWQiOiJyZXViZW4tdHVybmVyIn0.1iAdhkZjZDMHs-C16L7nmBJCk7YVdmENBuC3EZBG-9Y'),
+        DemoAppUser.gordon: const Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjMyNDgxMDEsInVzZXJfaWQiOiJnb3Jkb24taGF5ZXMifQ.Wtt563rhkv2oscAesiqNqN6z0nrwKQVrgdZkMTBftyM'),
+      }[this];
+}
---
+++ stream_agram/lib/components/upload_new_Post/upload.dart
@@ -0,0 +1,1 @@
+export 'upload_post_screen.dart';
